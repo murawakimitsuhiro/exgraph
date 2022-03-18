b30ccb14c819331bb23e35f44d40f796a2aabb56
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2022-03-19 01:33:52 +0900

    [add] color mode

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const title = "Hello Nuxt3!";
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Html lang="ja">
+  <Head>
+    <Title>exgraph | {{ title }}</Title>
+    <ColorModePicker />
+    <Meta name="description" :content="`This is ${title} page`" />
+  </Head>
+  </Html>
+
+  <h1>{{ title }}</h1>
 </template>
